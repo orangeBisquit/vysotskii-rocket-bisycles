216cb2efe6354df4dfcdab30baca60707063953a
Artem <theuselessn@gmail.com>
2021-07-27 19:41:07 +0600

    menu scroll fixed

--- source/js/script.js
+++ source/js/script.js
@@ -5,36 +5,60 @@ const header = document.querySelector('.page__header');
 const main = document.querySelector('.page__main');
 const footer = document.querySelector('.page__footer');
 const navLinks = document.querySelectorAll('.nav__item a');
+const page = document.querySelector('.page');
+// Menu
+// const scrollToTop = () => {
+//   window.scrollTo(0, 0);
+// };
 
-const scrollToTop = () => {
-  window.scrollTo(0, 0);
-};
+// const toggleClass = (element, className) => {
+//   element.classList.toggle(className);
+// };
 
-const toggleClass = (element, className) => {
-  element.classList.toggle(className);
-};
+// const hideScroll = () => {
+//   toggleClass(header, 'page__header--menu-open');
+//   toggleClass(main, 'page__main--menu-open');
+//   toggleClass(footer, 'page__footer--menu-open');
+// }
 
-const hideScroll = () => {
-  toggleClass(header, 'page__header--menu-open');
-  toggleClass(main, 'page__main--menu-open');
-  toggleClass(footer, 'page__footer--menu-open');
+// let isMenuActive = false;
+
+// const handleMenu = (evt) => {
+//   toggleClass(hamburger, 'hamburger--active');
+//   isMenuActive = !isMenuActive;
+
+//   if (isMenuActive) {
+//     toggleClass(nav, 'nav--active');
+//   }
+// };
+
+const menuOpenHandler = () => {
+  hamburger.classList.add('hamburger--active');
+  nav.classList.add('nav--active');
+  page.classList.add('page--scroll-disabled');
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', menuCloseHandler);
+  })
+
+  hamburger.removeEventListener('click', menuOpenHandler);
+  hamburger.addEventListener('click', menuCloseHandler);
 }
 
-const handleMenu = (evt) => {
-  toggleClass(hamburger, 'hamburger--active');
-  toggleClass(nav, 'nav--active');
-  hideScroll();
+const menuCloseHandler = () => {
+  hamburger.classList.remove('hamburger--active');
+  nav.classList.remove('nav--active');
+  page.classList.remove('page--scroll-disabled');
 
-  if (!hamburger.classList.contains('hamburger--active') && evt.target === hamburger) {
-    scrollToTop();
-  }
-};
+  navLinks.forEach((link) => {
+    link.removeEventListener('click', menuCloseHandler);
+  })
 
-navLinks.forEach((link) => {
-  link.addEventListener('click', handleMenu);
-})
+  hamburger.addEventListener('click', menuOpenHandler);
+  hamburger.removeEventListener('click', menuCloseHandler);
+}
 
-hamburger.addEventListener('click', handleMenu);
+hamburger.addEventListener('click', menuOpenHandler);
 
 nav.classList.remove('nav--nojs');
 hamburger.classList.remove('hamburger--no-js');
