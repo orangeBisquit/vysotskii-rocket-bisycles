bd383be0eb2c3817e02129df29ba485c6e47e649
Artem <theuselessn@gmail.com>
2021-07-27 20:23:51 +0600

    pre project review

--- build/js/script.js
+++ build/js/script.js
@@ -1,37 +1,14 @@
 'use strict';
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
-const header = document.querySelector('.page__header');
-const main = document.querySelector('.page__main');
-const footer = document.querySelector('.page__footer');
 const navLinks = document.querySelectorAll('.nav__item a');
 const page = document.querySelector('.page');
-// Menu
-// const scrollToTop = () => {
-//   window.scrollTo(0, 0);
-// };
 
-// const toggleClass = (element, className) => {
-//   element.classList.toggle(className);
-// };
+// No JS Remove
+nav.classList.remove('nav--nojs');
+hamburger.classList.remove('hamburger--no-js');
 
-// const hideScroll = () => {
-//   toggleClass(header, 'page__header--menu-open');
-//   toggleClass(main, 'page__main--menu-open');
-//   toggleClass(footer, 'page__footer--menu-open');
-// }
-
-// let isMenuActive = false;
-
-// const handleMenu = (evt) => {
-//   toggleClass(hamburger, 'hamburger--active');
-//   isMenuActive = !isMenuActive;
-
-//   if (isMenuActive) {
-//     toggleClass(nav, 'nav--active');
-//   }
-// };
-
+// Menu Handler
 const menuOpenHandler = () => {
   hamburger.classList.add('hamburger--active');
   nav.classList.add('nav--active');
@@ -60,5 +37,4 @@ const menuCloseHandler = () => {
 
 hamburger.addEventListener('click', menuOpenHandler);
 
-nav.classList.remove('nav--nojs');
-hamburger.classList.remove('hamburger--no-js');
+
